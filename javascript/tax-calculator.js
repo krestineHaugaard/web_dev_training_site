75ed3c6d945467f379aba23f285aa22ed07726f1
Krestine H <krha0001@stud.kea.dk>
2025-09-07 15:54:00 +0200

    tax calculator done

--- javascript/tax-calculator.js
+++ javascript/tax-calculator.js
@@ -1,1 +1,14 @@
+document.querySelector("#tax_form").addEventListener("submit", (e) => {
+  e.preventDefault();
 
+  const moneytaryAmount = document.querySelector("#txtMonetaryAmount").value;
+  const taxPercentage = document.querySelector("#txtTaxPercentage").value;
+
+  const usableTax = taxPercentage / 100;
+
+  const taxAmount = moneytaryAmount * usableTax;
+  const finalAmount = moneytaryAmount - taxAmount;
+
+  document.querySelector("#finalTaxAmount").innerHTML = taxAmount;
+  document.querySelector("#finalMoneyAmount").innerHTML = finalAmount;
+});
